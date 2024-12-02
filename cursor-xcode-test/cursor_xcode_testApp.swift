ecda97c58e17b19bd0dba098154906ea012c4f43
shuvamshr <shuvamshrestha408@gmail.com>
2024-12-03 10:13:33 +1100

    Initial Commit

---
+++ cursor-xcode-test/cursor_xcode_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  cursor_xcode_testApp.swift
+//  cursor-xcode-test
+//
+//  Created by Shuvam Shrestha on 3/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct cursor_xcode_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
